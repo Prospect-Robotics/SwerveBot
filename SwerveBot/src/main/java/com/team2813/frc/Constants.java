7fa416bcce8bb62689aae4386b33a349f5052e6e
abhineetpal <aip_embox@thepal.org>
2022-06-11 00:50:35 -0700

    Update Constants.java

--- SwerveBot/src/main/java/com/team2813/frc/Constants.java
+++ SwerveBot/src/main/java/com/team2813/frc/Constants.java
@@ -5,6 +5,8 @@
 
 package com.team2813.frc;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,4 +15,39 @@ package com.team2813.frc;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    // CAN IDs
+
+    // Swerve Modules
+
+    // Front Left
+    public static final int FRONT_LEFT_DRIVE_ID = 1;
+    public static final int FRONT_LEFT_STEER_ID = 2;
+    public static final int FRONT_LEFT_ENCODER_ID = 3;
+
+    // Front Right
+    public static final int FRONT_RIGHT_DRIVE_ID = 4;
+    public static final int FRONT_RIGHT_STEER_ID = 5;
+    public static final int FRONT_RIGHT_ENCODER_ID = 6;
+
+    // Back Left
+    public static final int BACK_LEFT_DRIVE_ID = 7;
+    public static final int BACK_LEFT_STEER_ID = 8;
+    public static final int BACK_LEFT_ENCODER_ID = 9;
+
+    // Back Right
+    public static final int BACK_RIGHT_DRIVE_ID = 10;
+    public static final int BACK_RIGHT_STEER_ID = 11;
+    public static final int BACK_RIGHT_ENCODER_ID = 12;
+
+    // Physical Drive Constants
+
+    // Steer offsets
+    public static final double FRONT_LEFT_STEER_OFFSET = -Math.toRadians(0); // FIXME Measure and set front left steer offset
+    public static final double FRONT_RIGHT_STEER_OFFSET = -Math.toRadians(0); // FIXME Measure and set front right steer offset
+    public static final double BACK_LEFT_STEER_OFFSET = -Math.toRadians(0); // FIXME Measure and set back left steer offset
+    public static final double BACK_RIGHT_STEER_OFFSET = -Math.toRadians(0); // FIXME Measure and set bakc right steer offset
+
+    public static final double TRACKWIDTH = Units.inchesToMeters(19.5); // meters
+    public static final double WHEELBASE = Units.inchesToMeters(21.5); // meters
+}
