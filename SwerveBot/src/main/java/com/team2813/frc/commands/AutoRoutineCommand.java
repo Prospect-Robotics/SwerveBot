52ce0f77b0573b308ec8c7a4d2562e56bbdb572e
abhineetpal <aip_embox@thepal.org>
2022-07-20 14:47:40 -0700

    Create AutoRoutineCommand.java
    Implements drive subsystem's initAutonomous() method in initialize() method.

---
+++ SwerveBot/src/main/java/com/team2813/frc/commands/AutoRoutineCommand.java
@@ -0,0 +1,54 @@
+package com.team2813.frc.commands;
+
+import com.team2813.frc.subsystems.Drive;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutoRoutineCommand extends SequentialCommandGroup {
+
+    private final Drive driveSubsystem;
+    private final Command[] commands;
+
+    private Pose2d initialPose;
+
+    public AutoRoutineCommand(Drive driveSubsystem, Command... commands) {
+        super(commands);
+
+        this.driveSubsystem = driveSubsystem;
+        this.commands = commands;
+    }
+
+    public AutoRoutineCommand(Drive driveSubsystem, Pose2d initialPose, Command... commands) {
+        super(commands);
+
+        this.driveSubsystem = driveSubsystem;
+        this.commands = commands;
+        this.initialPose = initialPose;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        if (initialPose != null) {
+            driveSubsystem.initAutonomous(initialPose);
+        }
+        else {
+            FollowCommand firstFollowCommand = null;
+            for (Command command : commands) {
+                if (command instanceof FollowCommand) {
+                    firstFollowCommand = (FollowCommand) command;
+                    break;
+                }
+            }
+
+            if (firstFollowCommand != null) {
+                driveSubsystem.initAutonomous(firstFollowCommand.getTrajectory().getInitialPose());
+            }
+            else {
+                throw new NullPointerException();
+            }
+        }
+    }
+}
