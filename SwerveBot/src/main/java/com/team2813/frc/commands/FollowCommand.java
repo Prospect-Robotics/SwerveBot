2b68c13b42f429673cd3c8e48d102c647d843160
abhineetpal <aip_embox@thepal.org>
2022-08-03 17:16:07 -0700

    Integrated theta controller
    Took the parameters of the theta controller in RotateCommand and put them in the theta controller in FollowCommand. Tested with advanced paths and it turned out good.

--- SwerveBot/src/main/java/com/team2813/frc/commands/FollowCommand.java
+++ SwerveBot/src/main/java/com/team2813/frc/commands/FollowCommand.java
@@ -25,9 +25,9 @@ public class FollowCommand extends PPSwerveControllerCommand {
     private static final PIDController xController = new PIDController(1.25, 0.15, 0);
     private static final PIDController yController = new PIDController(1, 0, 0);
     private static final ProfiledPIDController thetaController = new ProfiledPIDController(
+            1,
             0,
-            0,
-            0,
+            0.0225,
             new TrapezoidProfile.Constraints(Drive.MAX_ANGULAR_VELOCITY, Drive.MAX_ANGULAR_ACCELERATION)
     );
 
