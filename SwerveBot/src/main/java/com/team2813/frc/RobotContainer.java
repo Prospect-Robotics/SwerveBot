dd60a192a09666a930b01c6a5c47b96333f5ee1e
cuttestkittensrule <mangoiscute95@gmail.com>
2022-11-03 20:52:20 -0700

    "Yeah my code should be corr- wait neos are brushless?"

--- SwerveBot/src/main/java/com/team2813/frc/RobotContainer.java
+++ SwerveBot/src/main/java/com/team2813/frc/RobotContainer.java
@@ -19,31 +19,32 @@ import com.team2813.frc.subsystems.Drive;
 
 import static com.team2813.frc.Controls.*;
 
-
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
-public class RobotContainer
-{
+public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final Drive drive = new Drive();
     private final Magazine mag = new Magazine();
-    private final Intake intake = new intake();
+    private final Intake intake = new Intake();
 
     private final XboxController controller = new XboxController(0);
-    
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
-    public RobotContainer()
-    {
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
         drive.setDefaultCommand(new DefaultDriveCommand(
                 drive,
                 () -> -modifyAxis(controller.getLeftY()) * Drive.MAX_VELOCITY,
                 () -> -modifyAxis(controller.getLeftX()) * Drive.MAX_VELOCITY,
-                () -> -modifyAxis(controller.getRightX()) * Drive.MAX_ANGULAR_VELOCITY
-        ));
+                () -> -modifyAxis(controller.getRightX()) * Drive.MAX_ANGULAR_VELOCITY));
 
         // Configure the button bindings
         configureButtonBindings();
@@ -53,38 +54,35 @@ public class RobotContainer
     Drive getDrive() {
         return drive;
     }
-    
-    
+
     /**
-     * Use this method to define your button->command mappings. Buttons can be created by
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
-    private void configureButtonBindings()
-    {
+    private void configureButtonBindings() {
         // Add button to command mappings here.
-        // See https://docs.wpilib.org/en/stable/docs/software/commandbased/binding-commands-to-triggers.html
+        // See
+        // https://docs.wpilib.org/en/stable/docs/software/commandbased/binding-commands-to-triggers.html
         INTAKE_BUTTON.whenHeld(new ParallelCommandGroup(
-            new InstantCommand(mag::intake, mag),
-            new InstantCommand(intake::deployIntake, intake)
-        ));
+                new InstantCommand(mag::intake, mag),
+                new InstantCommand(intake::deployIntake, intake)));
         INTAKE_BUTTON.whenReleased(new ParallelCommandGroup(
-            new InstantCommand(mag::disable, mag),
-            new InstantCommand(intake::retractIntake, intake)
-        ));
+                new InstantCommand(mag::disable, mag),
+                new InstantCommand(intake::retractIntake, intake)));
         OUTTAKE_BUTTON.whenHeld(new InstantCommand(mag::outtake, mag));
         OUTTAKE_BUTTON.whenReleased(new InstantCommand(mag::disable, mag));
     }
-    
-    
+
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
      * @return the command to run in autonomous
      */
-    public Command getAutonomousCommand()
-    {
+    public Command getAutonomousCommand() {
         AutoRoutine selectedRoutine = ShuffleboardData.routineChooser.getSelected();
         return selectedRoutine.getCommand();
     }
@@ -99,12 +97,10 @@ public class RobotContainer
         if (Math.abs(value) > deadband) {
             if (value > 0) {
                 return (value - deadband) / (1 - deadband);
-            }
-            else {
+            } else {
                 return (value + deadband) / (1 - deadband);
             }
-        }
-        else {
+        } else {
             return 0;
         }
     }
