dee63a08f69e9d0918f0b6d030f87cf2dfa2d042
cuttestkittensrule <mangoiscute95@gmail.com>
2022-11-05 15:28:16 -0700

    Changed mag motor to a falcon

--- SwerveBot/src/main/java/com/team2813/frc/subsystems/Intake.java
+++ SwerveBot/src/main/java/com/team2813/frc/subsystems/Intake.java
@@ -1,25 +1,27 @@
 package com.team2813.frc.subsystems;
 
 import com.team2813.lib.motors.ControlMode;
-import com.team2813.lib.motors.SparkMaxWrapper;
+import com.team2813.lib.motors.TalonFXWrapper;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.team2813.lib.solenoid.SolenoidGroup;
 
 import static com.team2813.frc.Constants.*;
 
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Intake extends SubsystemBase {
     
     private static final double INTAKE_DEMAND = 0.67;
 
-    SparkMaxWrapper motor;
+    TalonFXWrapper motor;
     SolenoidGroup pistons;
 
     public Intake() {
-        motor = new SparkMaxWrapper(INTAKE_MOTOR_ID, MotorType.kBrushless, true);
+        motor = new TalonFXWrapper(INTAKE_MOTOR_ID, TalonFXInvertType.Clockwise);
         pistons = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, LEFT_PISTON_PORT, RIGHT_PISTON_PORT);
     }
 
