2fff4ad37fbf0c26d702d41658db518f59079b24
cuttestkittensrule <mangoiscute95@gmail.com>
2022-11-05 17:32:07 -0700

    whoops

---
+++ SwerveBot/src/main/java/com/team2813/frc/commands/AutoLowShootCommand.java
@@ -0,0 +1,17 @@
+package com.team2813.frc.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import com.team2813.frc.subsystems.Shooter;
+import com.team2813.frc.subsystems.Magazine;
+
+public class AutoLowShootCommand extends SequentialCommandGroup {
+    public AutoLowShootCommand(Shooter shooter, Magazine mag) {
+        super(
+            new InstantCommand(shooter::lowShoot, shooter),
+            new WaitCommand(0.25),
+            new InstantCommand(mag::intake, mag)
+        );
+    }
+}
