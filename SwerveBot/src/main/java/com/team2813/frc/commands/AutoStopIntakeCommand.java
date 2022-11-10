8098fcb1262150f86b19c40818cc431f0cd3568d
abhineetpal <aip_embox@thepal.org>
2022-11-10 00:09:15 -0800

    first-test tweaks (not complete)
    We are still testing things, so there will be more tweaks to come

--- SwerveBot/src/main/java/com/team2813/frc/commands/AutoStopIntakeCommand.java
+++ SwerveBot/src/main/java/com/team2813/frc/commands/AutoStopIntakeCommand.java
@@ -3,20 +3,24 @@ package com.team2813.frc.commands;
 import com.team2813.frc.subsystems.Intake;
 import com.team2813.frc.subsystems.Magazine;
 
+import com.team2813.frc.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoStopIntakeCommand extends SequentialCommandGroup {
 
-    public AutoStopIntakeCommand(Intake intakeSubsystem, Magazine magSubsystem) {
+    public AutoStopIntakeCommand(Intake intakeSubsystem, Magazine magSubsystem, Shooter shooterSubsystem) {
         super(
                 new ParallelCommandGroup(
                         // Stops the intake
                         new InstantCommand(intakeSubsystem::stopIntake, intakeSubsystem),
 
                         // Stops the mag
-                        new InstantCommand(magSubsystem::disable, magSubsystem)),
+                        new InstantCommand(magSubsystem::disable, magSubsystem),
+
+                        // Stops the shooter
+                        new InstantCommand(shooterSubsystem::disable, shooterSubsystem)),
 
                 new InstantCommand(intakeSubsystem::retractIntake, intakeSubsystem));
     }
--- SwerveBot/src/main/java/com/team2813/frc/subsystems/Shooter.java
+++ SwerveBot/src/main/java/com/team2813/frc/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package com.team2813.frc.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.team2813.lib.motors.TalonFXWrapper;
 import static com.team2813.frc.Constants.*;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
@@ -9,12 +10,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
     TalonFXWrapper shooter;
-    private static final double LOW_SHOOT_DEMAND = 0.25;
-    private static final double HIGH_SHOOT_DEMAND = 0.75;
+    private static final double HIGH_SHOOT_DEMAND = 0.4;
+    private static final double LOW_SHOOT_DEMAND = 0.15;
+    private static final double KICK_DEMAND = -0.1;
 
     public Shooter() {
         shooter = new TalonFXWrapper(SHOOTER_MOTOR_FRONT_ID, TalonFXInvertType.CounterClockwise);
         shooter.addFollower(SHOOTER_MOTOR_BACK_ID, TalonFXInvertType.FollowMaster);
+        shooter.setNeutralMode(NeutralMode.Brake);
     }
 
     public void lowShoot() {
@@ -27,5 +30,9 @@ public class Shooter extends SubsystemBase {
 
     public void disable() {
         shooter.set(ControlMode.DUTY_CYCLE, 0);
+    }
+
+    public void kick() {
+        shooter.set(ControlMode.DUTY_CYCLE, KICK_DEMAND);
     }
 }
