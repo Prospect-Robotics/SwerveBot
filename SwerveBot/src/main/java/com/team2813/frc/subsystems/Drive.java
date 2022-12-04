8b96015b33a0f311851a547b718e8c46470c7bab
abhineetpal <aip_embox@thepal.org>
2022-12-03 16:44:36 -0800

    reduced max drive speed
    We are planning on dragging a cooler behind the bot, so we want the bot to drive slower.

--- SwerveBot/src/main/java/com/team2813/frc/subsystems/Drive.java
+++ SwerveBot/src/main/java/com/team2813/frc/subsystems/Drive.java
@@ -28,7 +28,7 @@ public class Drive extends SubsystemBase {
 
     public static final double MAX_VELOCITY = 6380.0 / 60.0 *
             SdsModuleConfigurations.MK4_L2.getDriveReduction() *
-            WHEEL_CIRCUMFERENCE; // m/s
+            WHEEL_CIRCUMFERENCE / 2.0; // m/s
     public static final double MAX_ANGULAR_VELOCITY = MAX_VELOCITY / Math.hypot(TRACKWIDTH / 2, WHEELBASE / 2); // radians per second
     public static final double MAX_ANGULAR_ACCELERATION = Math.PI;
 
